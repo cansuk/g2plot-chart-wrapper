1923fbfd5a03e4ea8249321ea970f96df10a3052
cansuk <cansukaluc234@gmail.com>
2021-08-28 14:39:26 +0300

    #1 package build ve npme yayınlanmadan test projesine eklenmesi

---
+++ index.js
@@ -0,0 +1,3 @@
+import Badge from './src/components/Badge';
+import Button from './src/components/Button';
+export { Badge, Button };
---
+++ src/components/Badge.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Badge = (props) => {
+    return (
+        <div className={`badge ${!props.value ? 'badge--none' : ''} `}>
+            <h4 className="heavy">{props.value || 0}</h4>
+        </div>
+    )
+}
+export default Badge;
